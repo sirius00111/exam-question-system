c02483c8ba7a698746ce7904d99fe6d68df8ec41
sirius00111 <168199890+sirius00111@users.noreply.github.com>
2025-06-25 00:54:58 +0800

    Update ExamQuestionSystemApplication.java

--- back end/src/main/java/com/sirius/ExamQuestionSystemApplication.java
+++ back end/src/main/java/com/sirius/ExamQuestionSystemApplication.java
@@ -1,4 +1,4 @@
-// deepseek api     名称:sirius-01        密钥:sk-29386c87e34a4d6fa574f9a46e571a1a
+// deepseek api     名称:sirius-01        密钥:sk-29386***********************1a1a
 
 package com.sirius;
 
